c00a8707ffd80ca75a0724141d3669559ef5f607
usama-987 <usamas406@gmail.com>
2026-04-17 17:53:48 +0500

    Making the routes file

---
+++ backend/src/routes/interview.routes.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const authMiddleware = require("../middlewares/auth.middleware")
+const interviewController = require("../controllers/interview.controller")
+const upload = require("../middlewares/file.middleware")
+
+const interviewRouter = express.Router()
+
+
+
+
+interviewRouter.post("/", authMiddleware.authUser, upload.single("resume"), interviewController.generateInterViewReportController)
+
+
+interviewRouter.get("/report/:interviewId", authMiddleware.authUser, interviewController.getInterviewReportByIdController)
+
+
+interviewRouter.get("/", authMiddleware.authUser, interviewController.getAllInterviewReportsController)
+
+
+
+interviewRouter.post("/resume/pdf/:interviewReportId", authMiddleware.authUser, interviewController.generateResumePdfController)
+
+
+
+module.exports = interviewRouter
